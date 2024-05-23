f2237f36258a480a5647034cb1f9b140e4ea0ce0
zhangzhihui <1152947493@qq.com>
2024-05-23 15:54:53 +0800

    msg_type.toml生成代码工具

---
+++ config/lua/message_def.lua
@@ -0,0 +1,12 @@
+
+-- Generate by tools, Do not Edit.
+
+_G.MESSAGE_TYPE = {
+	GS2C_COMMON_ERR = {ID = 100, Msg = "pb.CommonErrResp"}, -- 错误信息统一返回
+
+	C2GS_LOGIN = {ID = 500, Msg = "pb.LoginReq"}, -- 玩家登录请求,根据IsGuest区分是否游客登录
+	GS2C_LOGIN = {ID = 501, Msg = "pb.LoginResp"}, -- 玩家登录返回
+
+
+}
+
---
+++ tools/source/gen_client_jsmsgdef.py
@@ -0,0 +1,39 @@
+import re
+
+input = "../resources/def/toml/msg_type.toml"
+output = "../resources/js/message_def.js"
+
+pattern_outer = re.compile(r'{\s*begin\s*=\s*(\d+),\s*fields\s*=\s*\[([\s\S]*?)\]\s*}', re.MULTILINE)
+pattern_inner = re.compile(r'{\s*name\s*=\s*"([^"]+)",\s*msg\s*=\s*"([^"]+)",\s*comment\s*=\s*"([^"]+)"\s*}')
+
+content = """
+// Generate by tools, Do not Edit.
+const root = require('game_msg.js');
+
+const MESSAGE_TYPE = {
+%s
+};
+"""
+
+if __name__ == '__main__':
+	with open(input, "r", encoding="utf-8") as f:
+		text = f.read()
+
+	tmps = ""
+	matches = pattern_outer.findall(text)
+	for match in matches:
+		begin, fields = match[0], match[1]
+		matches_inner = pattern_inner.findall(fields)
+		i = 0
+		for match_inner in matches_inner:
+			name = match_inner[0]
+			msg = match_inner[1]
+			comment = match_inner[2]
+			tmps += f"\t{name}: {{ID: {int(begin) + i}, Msg: root.{msg}}}, // {comment}\n"
+			i += 1
+		tmps += "\n"
+
+	with open(output, "w", encoding="utf-8") as f:
+		s = content % tmps
+		f.write(s)
+		print(s)
