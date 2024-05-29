6f764616be24bd4e75a9187ea39be3a4995bc309
zhangzhihui <1152947493@qq.com>
2024-05-29 18:25:17 +0800

    修改起服流程

--- service/main.lua
+++ service/main.lua
@@ -1,36 +1,21 @@
-local skynet = require "skynet"
+local skynet = require "skynet.manager"
 local runconfig = require "runconfig"
 local cluster = require "skynet.cluster"
 local log = require "log"
 
--- local bootstrap_wait = 30
-local bootstrap_wait = 10
-
--- 同步启动，保证启动完成才往下执行.避免还没启动完成消息已经来了
-local function sync_bootstrap(srv, name, id)
-    log.debug(string.format("sync_bootstrap start. srv:%s name:%s id:%s", srv, name, id))
-
-    local loopcnt = 0
+local function _bootstrap(srv, name, id)
+    log.debug(string.format("_bootstrap start. srv:%s name:%s id:%s", srv, name, id))
     local addr = skynet.newservice(srv, name, id)
-    while true do
-        local bootstraped = skynet.call(addr, "lua", "is_bootstraped")
-        log.info(string.format("sync_bootstrap start. srv:%s name:%s id:%s bootstraped:%s", srv, name, id, bootstraped))
-        if bootstraped then
-            log.info(string.format("sync_bootstrap success. srv:%s name:%s id:%s", srv, name, id))
-            return true
-        end
-        loopcnt = loopcnt + 1
-        if loopcnt >= bootstrap_wait then -- 60秒还没启动完成
-            assert(nil, string.format("sync_bootstrap fail. srv:%s name:%s id:%s", srv, name, id))
-            return false
-        end
-        skynet.sleep(SKYNET_SECOND)
+    if addr == nil then
+        log.error(string.format("_bootstrap fail. srv:%s name:%s id:%s", srv, name, id))
+        skynet.abort()
     end
 end
 
 skynet.start(
+    -- 顺序启动服务，任何一个服务启动失败则退出进程
     function()
-        sync_bootstrap("logger", "logger", nil) -- 启动logger服务
+        _bootstrap("logger", "logger", nil) -- 启动logger服务
 
         local selfnode = skynet.getenv("node")
         log.info("[--------start bootstrap main--------] node: ", selfnode)
@@ -39,20 +24,24 @@ skynet.start(
         cluster.open(selfnode) -- 打开当前节点
 
         if selfnode == runconfig.unique.login_mgr.node then
-            local addr = skynet.uniqueservice("login_mgr", "login_mgr", runconfig.unique.login_mgr.id)
+            local addr = skynet.uniqueservice("login_mgr", "login_mgr", nil)
+            if addr == nil then
+                log.error("_bootstrap login_mgr fail. srv:%s name:%s id:%s")
+                skynet.abort()
+            end
             cluster.register("login_mgr", addr) -- 注册login_mgr服务，不用.前缀，其他节点可以通过这个别名直接通信
         end
 
-        skynet.newservice("debug_console", runconfig.debug_console[selfnode].port) -- 启动debug_console服务
+        _bootstrap("debug_console", runconfig.debug_console[selfnode].port) -- 启动debug_console服务
 
         local cfgnode = runconfig[selfnode]
         for _, info in ipairs(cfgnode) do -- 顺序启动
             for id, _ in ipairs(info.list) do
-                sync_bootstrap(info.name, info.name, id) -- 本节点服务，服务内部自己注册别名
+                _bootstrap(info.name, info.name, id) -- 本节点服务，服务内部自己注册别名
             end
         end
 
-        sync_bootstrap("signal_handler", "signal_handler", nil) -- 启动信号捕捉服务
+        _bootstrap("signal_handler", "signal_handler", nil) -- 启动信号处理服务
 
         log.info("[--------end bootstrap main--------] node: ", selfnode)
         skynet.exit() -- 退出当前服务
